8b3880a8d4cd28c39260dae57c075618982281ae
danielele77 <dendy1998@azet.sk>
2023-10-26 18:33:55 +0200

    Initial commit

---
+++ src/main/java/sk/fei/asos/musichub/ServletInitializer.java
@@ -0,0 +1,13 @@
+package sk.fei.asos.musichub;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MusichubApplication.class);
+	}
+
+}
---
+++ src/test/java/sk/fei/asos/musichub/MusichubApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.fei.asos.musichub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusichubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
