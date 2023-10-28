75bfaf077a3024251d7a53d6ee16d19845ce5f52
Adrián Vrbovský <avrbovsky64@gmail.com>
2023-10-28 17:40:33 +0200

    Removed unused imports

--- src/main/java/sk/fei/asos/musichub/models/Playlist.java
+++ src/main/java/sk/fei/asos/musichub/models/Playlist.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 @Entity
--- src/main/java/sk/fei/asos/musichub/models/Song.java
+++ src/main/java/sk/fei/asos/musichub/models/Song.java
@@ -3,11 +3,8 @@ package sk.fei.asos.musichub.models;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity
