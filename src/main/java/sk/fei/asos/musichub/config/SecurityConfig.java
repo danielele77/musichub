0a91a67c5dd23e1f2a0461659881a59794c1cb26
danielele77 <dendy1998@azet.sk>
2023-11-15 22:15:12 +0100

    chore: 1:16 doesn't work, need to investigate ┌─────┐ |  jwtAthFilter defined in file [D:\Coding\Java\musichub\target\classes\sk\fei\asos\musichub\config\JwtAthFilter.class] ↑     ↓ |  securityConfig defined in file [D:\Coding\Java\musichub\target\classes\sk\fei\asos\musichub\config\SecurityConfig.class] └─────┘

--- src/main/java/sk/fei/asos/musichub/config/SecurityConfig.java
+++ src/main/java/sk/fei/asos/musichub/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package sk.fei.asos.musichub.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -24,6 +27,7 @@ import java.util.List;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
+@Component
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtAthFilter jwtAthFilter;
@@ -61,6 +65,14 @@ public class SecurityConfig {
         return authenticationProvider;
     }
 
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -77,3 +89,4 @@ public class SecurityConfig {
     }
 
 }
+
