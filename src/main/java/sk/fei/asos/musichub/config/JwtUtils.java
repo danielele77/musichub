886c2eaff230d2bb7e381b8dfeb5d93c0c529479
danielele77 <dendy1998@azet.sk>
2023-11-15 19:43:13 +0100

    feat: implement JwtUtils

--- src/main/java/sk/fei/asos/musichub/config/JwtUtils.java
+++ src/main/java/sk/fei/asos/musichub/config/JwtUtils.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 @Component
@@ -26,6 +27,13 @@ public class JwtUtils {
         return extractClaim(token, Claims::getExpiration);
     }
 
+    public boolean hasClaim(String token, String claimName){
+        final Claims claims = extractAllClaims(token);
+        return  claims.get(claimName) != null;
+    }
+
+
+
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
@@ -41,15 +49,22 @@ public class JwtUtils {
 
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
-        return createToken(claims, userDetails.getUsername());
+        return createToken(claims,userDetails);
     }
 
-    private String createToken(Map<String, Object> claims, String subject) {
+    private String createToken(Map<String, Object> claims, UserDetails userDetails) {
 
         return Jwts.builder().setClaims(claims).
-                setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                setSubject(userDetails.getUsername()).
+                claim("authorities",userDetails.getAuthorities()).
+                setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(24)))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+    }
+
+    public Boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())&& !isTokenExpired(token));
     }
 
 }
