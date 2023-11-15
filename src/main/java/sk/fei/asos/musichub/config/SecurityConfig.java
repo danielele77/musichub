6a93f9cd0b9be42c1027f9a28cd01ce3e36bea5e
danielele77 <dendy1998@azet.sk>
2023-11-15 21:13:24 +0100

    feat: implement passwordEncoder and AuthenticationProvider

--- src/main/java/sk/fei/asos/musichub/config/SecurityConfig.java
+++ src/main/java/sk/fei/asos/musichub/config/SecurityConfig.java
@@ -2,18 +2,22 @@ package sk.fei.asos.musichub.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -26,12 +30,12 @@ public class SecurityConfig {
     private final static List<UserDetails> APPLICATION_USERS = Arrays.asList(
             new User(
                     "musichub@gmail.com",
-                    "password",
+                    "$2a$12$MnZKESMHhV.h3evEomD1qui8YKis7Qg.0UcRbPBECxoVMkW4It7jq",
                     Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))
             ),
             new User(
                     "user.mail@gmail.com",
-                    "password",
+                    "$2a$12$MnZKESMHhV.h3evEomD1qui8YKis7Qg.0UcRbPBECxoVMkW4It7jq",
                     Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
             )
 
@@ -41,10 +45,26 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
                 .addFilterBefore(jwtAthFilter, UsernamePasswordAuthenticationFilter.class)
+                .authenticationProvider(authenticationProvider())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(withDefaults())
                 .formLogin(withDefaults());
         return http.build();
 
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+//        return NoOpPasswordEncoder.getInstance();
     }
 
     @Bean
