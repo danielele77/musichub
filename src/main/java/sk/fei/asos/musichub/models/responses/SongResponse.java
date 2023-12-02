55920eeb8433d43dfbf5236eeb9e6b3274dd229b
Roman Micuda <micuda.roman@hotmail.com>
2023-12-02 16:45:47 +0100

    fix: song response

--- src/main/java/sk/fei/asos/musichub/models/responses/SongResponse.java
+++ src/main/java/sk/fei/asos/musichub/models/responses/SongResponse.java
@@ -1,4 +1,48 @@
 package sk.fei.asos.musichub.models.responses;
 
+import jakarta.persistence.*;
+import sk.fei.asos.musichub.models.Genre;
+import sk.fei.asos.musichub.models.Playlist;
+import sk.fei.asos.musichub.models.Song;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class SongResponse {
+
+
+    private Long id;
+
+
+    private String name;
+
+
+    private String lyrics;
+
+
+    private String artist;
+
+
+    private String coverPhoto;
+
+
+    private String sourceName;
+
+
+
+    private Genre genre;
+
+
+    private Set<PlaylistResponse> playlists;
+
+    public SongResponse(Song song) {
+        this.id = song.getId();
+        this.name = song.getName();
+        this.lyrics = song.getLyrics();
+        this.artist = song.getArtist();
+        this.coverPhoto = song.getCoverPhoto();
+        this.genre = song.getGenre();
+        this.playlists = song.getPlaylists().stream().map(PlaylistResponse::new).collect(Collectors.toSet());
+    }
 }
